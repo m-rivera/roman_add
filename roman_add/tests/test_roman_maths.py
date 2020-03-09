17d2b898c1c4948190f47ab002efa1178c51b918
Miguel Rivera <m.rivera@qmul.ac.uk>
2020-03-09 12:40:26 +0000

    Add set of test numbers

--- roman_add/tests/test_roman_maths.py
+++ roman_add/tests/test_roman_maths.py
@@ -1,1 +1,8 @@
 """Unit tests for converting between Toman and Arabic numerals."""
+
+# sample numbers
+num_equiv = (('I',1),
+            ('IV',4),
+            ('XIX',19),
+            ('CMXCIX',999)
+            ('MMMCCX',3210))
