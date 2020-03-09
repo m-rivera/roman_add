1af2a893a7f1933ffbe233d085ecb4102e5c7d9c
Miguel Rivera <m.rivera@qmul.ac.uk>
2020-03-09 13:06:10 +0000

    Test converting Roman to Arabic numerals

--- roman_add/tests/test_roman_maths.py
+++ roman_add/tests/test_roman_maths.py
@@ -11,5 +11,12 @@ num_equiv = (('I',1),
 
 @pytest.mark.parametrize("rom,ara", num_equiv)
 def test_ara_to_rom(rom,ara):
+    """Test Arabic -> Roman"""
     calc_rom = rm.ara_to_rom(ara)
     assert calc_rom == rom
+
+@pytest.mark.parametrize("rom,ara", num_equiv)
+def test_rom_to_ara(rom,ara):
+    """Test Roman -> Arabic"""
+    calc_ara = rm.rom_to_ara(rom)
+    assert calc_ara == ara
