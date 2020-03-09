f2b8a0869f1d6a6db332e12612291192cdfa0b28
Miguel Rivera <m.rivera@qmul.ac.uk>
2020-03-09 12:49:28 +0000

    Add fundamental conversion chart

--- roman_add/src/roman_maths.py
+++ roman_add/src/roman_maths.py
@@ -1,1 +1,17 @@
 """Module compiling functions to convert between arabic and roman numerals."""
+
+# conversion chart between atomic Roman numerals and Arabic equivalents
+# not using just a dictionary since they are unordered before Python 3.7
+conv_chart = (('M',1000),
+                ('CM',900),
+                ('D',500),
+                ('CD',400),
+                ('C',100),
+                ('XC',90),
+                ('L',50),
+                ('XL',40),
+                ('X',10),
+                ('IX',9),
+                ('V',5),
+                ('IV',4),
+                ('I',1))
